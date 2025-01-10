44ca0aca12b67fd2fd43bf685decc8eaa8b3bc76
du-mozzie <55873131+du-mozzie@users.noreply.github.com>
2025-01-10 06:56:27 +0000

    Deploying to gh-pages from @ du-mozzie/du-mozzie.github.io@822db2c01fe9864357533050ad0ca781b5d51e5f 🚀

--- assets/js/google-ads.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-(function (c, l, a, r, i, t, y) {
-    c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
-    t = l.createElement(r); t.async = 1; t.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2273796902744859";
-    y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
-})(window, document, "adsbygoogle", "script", "ca-pub-2273796902744859");
